e98a9bf9c535f766862519f7f6c07bba3c594de1
Unknown <ap682814@ohio.edu>
2017-03-02 12:45:11 -0500

    Different way of scripting

--- AnimationTutorial/Assets/DoorController.cs
+++ AnimationTutorial/Assets/DoorController.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class DoorController : MonoBehaviour {
 
-	Animator animator;
+	//Animator animator;
+	public Rect openDoorRectangle;
+	public Rect closeDoorRectangle;
+	public Animator animator;
 
 
-
-
+/*
 	void Start () 
 	{
 		animator = GetComponent <Animator>();
-	}
+	}*/
 	
 
-	void OnGUI () 
+/*	void OnGUI () 
 	{
 		if (GUILayout.Button("Open Door"))
 		{
@@ -29,5 +31,22 @@ public class DoorController : MonoBehaviour {
 			animator.SetInteger ("Door Close", 1);
 			animator.SetInteger ("Door Open", 0);
 		}
+	}*/
+
+	void OnGUI ()
+	{
+		if(GUI.Button (openDoorRectangle, "Open Door"))
+		{
+			animator.SetInteger ("Door Open", 1);
+			animator.SetInteger ("Door Close", 0);
+		}
+
+		if(GUI.Button (closeDoorRectangle, "Close Door"))
+		{
+			animator.SetInteger ("Door Open", 0);
+			animator.SetInteger ("Door Close", 1);
+		}
 	}
+
+
 }
